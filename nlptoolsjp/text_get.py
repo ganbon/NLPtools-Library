0a2afadffa5955a4d0e240b192979b60f42eef4f
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-12-02 10:29:00 +0900

    不必要なファイル削除&変更

--- nlptoolsjp/text_get.py
+++ nlptoolsjp/text_get.py
@@ -15,7 +15,25 @@ def scraping(url, file_path = None):
         file_create(text,file_path)
     time.sleep(1)
     return text.split('\n')
-        
+
+def scraping_obj(url):
+    responses = requests.get(url)
+    soup = BeautifulSoup(responses.text, 'html.parser')
+    return soup
+
+def scraping_url(url,class_name,file_path = None):
+    try:
+        responses = requests.get(url)
+        soup = BeautifulSoup(responses.text, 'html.parser')
+    except:
+        return []
+    class_context = soup.find_all("a",class_=class_name)
+    title_list = [con_text.text for con_text in class_context]
+    url_list = [con_text.get("href") for con_text in class_context]
+    if file_path is not None:
+        file_create(class_context,file_path)
+    return url_list,title_list
+
 if __name__=="__main__":
     url = input()
-    scraping(url,f"data/test.txt")
+    scraping(url)
