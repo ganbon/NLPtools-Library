8424b07aef4c052aec69744d5acb022b588899b4
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-12-02 10:42:00 +0900

    依存ライブラリ記述

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+install_requires = [
+       "mecab-python3", 
+        "ipadic",
+        "pandas"
+]
 
 setup(
     name = 'nlptoolsjp',
     version = '0.3',
     author = 'ganbon', 
+    install_requires=install_requires,
     packages = find_packages()
 )
